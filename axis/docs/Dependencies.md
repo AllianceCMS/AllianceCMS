e7381267f27c43a794e17b939e5698b9198cfc57
Burnsy <github@jbwebware.com>
2013-07-02 14:37:12 -0400

    Updated Dependencies.md
    Saving changes so I can move this code to official dev branch

--- axis/docs/Dependencies.md
+++ axis/docs/Dependencies.md
@@ -1,6 +1,6 @@
 * ADOdb - http://adodb.sourceforge.net/ (Soon to be replaced)
 * Aura.Autoloader - http://auraphp.com/Aura.Autoload/
 * Aura.Router - http://auraphp.com/Aura.Router/
-* Aura.View - http://auraphp.com/Aura.View/ (not in use yet)
+* (not in use yet) Aura.View - http://auraphp.com/Aura.View/
 * Gaufrette - https://github.com/KnpLabs/gaufrette (haven't used 'composer install' yet, just downloaded zip from Github Master)
-* Normalize.css (http://necolas.github.io/normalize.css/)
+* (not in use yet) Normalize.css (http://necolas.github.io/normalize.css/)
--- axis/tests/gaufrette.php
+++ axis/tests/gaufrette.php
@@ -3,6 +3,16 @@
 use Gaufrette\Filesystem;
 use Gaufrette\Adapter\Local as LocalAdapter;
 
+$adapter = new LocalAdapter(PLUGINS_AXIS);
+$pluginsAxis = new Filesystem($adapter);
+
+echo '<br /><pre>';
+echo print_r($pluginsAxis->keys('home'));
+echo '</pre><br />';
+
+//echo '<br />File exists? ' . $pluginsAxis->read('PLACEHOLDER.md') . '<br />';
+
+/*
 $adapter = new LocalAdapter(TESTS . 'var/');
 $filesystem = new Filesystem($adapter);
 
@@ -11,3 +21,4 @@ $filesystem = new Filesystem($adapter);
 $content = 'Hello I am the new content';
 
 $filesystem->write('test_02.md', $content);
+//*/
