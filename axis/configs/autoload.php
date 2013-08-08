31a6f3573b2adcc5b10c259615a6ceac9ba20890
jburns131 <github@jbwebware.com>
2013-08-08 03:44:17 -0400

    Renamed autoload.php

---
+++ axis/configs/autoload.php
@@ -0,0 +1,13 @@
+<?php
+
+// Set Paths for AuraPHP's Autoloader
+$acmsLoader = require PACKAGES . 'Aura.Autoload' . DS . 'scripts' . DS . 'instance.php';
+$acmsLoader->register();
+
+$acmsLoader->setPaths([
+    'Acms\Core\\' => PACKAGES . 'Acms.Core' . DS . 'src' . DS,
+    'Aura\Router\\' => PACKAGES . 'Aura.Router' . DS . 'src' . DS,
+    'Aura\Session\\' => PACKAGES . 'Aura.Session' . DS . 'src' . DS,
+    'Aura\Sql\\' => PACKAGES . 'Aura.Sql' . DS . 'src' . DS,
+//    'Symfony\\' => PACKAGES . 'Symfony' . DS . 'src' . DS,
+]);
