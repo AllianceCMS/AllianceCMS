097518d8c9b5a92b477aa253a5916cc330ad0869
jburns131 <jburns131@gmail.com>
2014-01-12 03:04:15 -0500

    Initial commit for Brownie theme port

---
+++ public_html/themes/Brownie/install.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Theme Details
+ *
+ * Used for Theme installation
+ */
+
+$details = [
+	'name' => 'Brownie',
+	'version' => '0.01',
+	'description' => 'This is a port of the "Brownie" template by eGrappler.com.',
+	'artist' => 'Jesse Burns',
+	'artist_email' => 'jesse.burns@alliancecms.com',
+	'artist_site' => 'http://www.alliancecms.com',
+	'templates' => [
+	   'home' => 'home.tpl.php',
+	   /*
+	   'blog_list' => '',
+	   'blog_post' => '',
+	   //*/
+	   'two_colum_left' => 'two_colum_left.tpl.php',
+	   'two_colum_right' => 'two_colum_right.tpl.php',
+	   'three_column' => 'three_column.tpl.php',
+	   'full_width' => 'full_width.tpl.php',
+	   '404' => '404.tpl.php',
+    ],
+];
